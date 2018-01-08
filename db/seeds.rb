86c1f0995c75039bda5b68c084aa90c15357a377
mikefsm <sftmfs@gmail.com>
2018-01-08 09:32:42 -0500

    Added seed file and Faker gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+  Blog.create!(
+    title: Faker::HarryPotter.character,
+    body: Faker::HarryPotter.quote 
+  )
+end
+
+
+5.times do |skill|
+  Skill.create!(
+    title: Faker::Beer.name,
+    percent_utilized: Faker::Number.between(1, 99)
+  )
+end
+
+10.times do |portfolio|
+  Portfolio.create!(
+    title: Faker::StarWars.vehicle,
+    subtitle: Faker::StarWars.specie,
+    body: Faker::StarWars.quote,
+    main_image: 'http://via.placeholder.com/600x400',
+    thumb_image: 'http://via.placeholder.com/350x200'
+  )
+end
